103cb0e6e3825252701fbea8724399e28a6f805b
Remy Queny <remy.queny@etu.isima.fr>
2026-06-22 15:45:33 +0200

    add test_connexion

---
+++ test_connexion.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.getOrCreate()
+
+storage_account_name = "stsncfdataremy"
+secret_scope_name = "azure_vault"
+secret_key_name = "datalake-access-key"
+
+print("Récupération de la clé d'accès depuis la key vault...")
+
+storage_account_key = dbutils.secrets.get(scope=secret_scope_name, key=secret_key_name)
+
+spark.conf.set(f"fs.azure.account.key.{storage_account_name}.dfs.core.windows.net",storage_account_key)
+print("Connexion etablie avec Datalake.")
+
+path_bronze = f"abfss://stsncfdataremy.blob.core.windows.net/datalake/1-bronze/sncf_historique/regularite-mensuelle-tgv-aqst.csv"
+print(f"Voici le chemin du fichier {path_bronze}")
+print("En atttente de l'analyse du fichier CSV...")
