48d1c2f977fbd9a52ac704f6b4c45691e606d16a
Remy Queny <remy.queny@etu.isima.fr>
2026-06-22 16:39:20 +0200

    ajout reset spark dans test_connexion

--- test_connexion.py
+++ test_connexion.py
@@ -3,6 +3,13 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.getOrCreate()
 
 storage_account_name = "stsncfdataremy"
+
+try:
+    spark.conf.unset(f"fs.azure.account.key.{storage_account_name}.dfs.core.windows.net")
+    print("🧹 Mémoire de session nettoyée (configuration fantôme effacée) !")
+except Exception:
+    pass
+
 path_bronze = f"abfss://datalake@{storage_account_name}.dfs.core.windows.net/1-bronze/sncf_historique/regularite-mensuelle-tgv-aqst.csv"
 
 print(f"Tentative de lecture directe du fichier : {path_bronze}")
